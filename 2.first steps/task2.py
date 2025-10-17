7a53253e617ad999089b3a4cd9c67280b879a5a3
gabstersPie <gabriella.iofe@protonmail.com>
2025-10-17 18:16:08 +0200

    add task 2&3 solutions

---
+++ 2.first steps/task2.py
@@ -0,0 +1,10 @@
+#hello world
+print("hello world")
+
+#hello world, banana, orange, potato
+print("hello world", "banana", "orange", "potato", sep=",")
+#hello world - banana - orange - potato
+print("hello world", "banana", "orange", "potato", sep="-")
+
+#hello world!
+print(" hello world", end="!")
