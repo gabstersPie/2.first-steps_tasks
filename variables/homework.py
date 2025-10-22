9c9f03f25ffd7e469d171bab470c58625ede4627
gabstersPie <gabriella.iofe@protonmail.com>
2025-10-22 18:42:57 +0200

    fninished homework variables

---
+++ variables/homework.py
@@ -0,0 +1,23 @@
+# Task 1
+my_name = "gabriella"
+weekday = "wednesday"
+
+print(f"Good day {my_name.title()}! {weekday.title()} is a perfect day to learn python.")
+
+# Task 2
+first_name =  "gabriella"
+last_name = "iofe"
+
+print("Hello" + first_name + last_name)
+
+# Task 3
+a = 1
+b = 2
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Division:", a / b)
+print("Multiplication:", a * b)
+print("Exponent (Power):", a ** b)
+print("Modulus:", a % b)
+print("Floor division:", a // b)
