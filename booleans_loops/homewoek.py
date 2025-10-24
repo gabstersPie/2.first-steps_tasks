431d2c792e587bcfa35c8c832194dff1eea7c6be
gabstersPie <gabriella.iofe@protonmail.com>
2025-10-24 17:50:21 +0200

    created a program that prints the first and last 2 charachters of the user input

--- booleans_loops/homewoek.py
+++ booleans_loops/homewoek.py
@@ -1,1 +1,11 @@
-test test
+# Task 1
+vegetable = input("enter your favorite vegetable here:")
+if len(vegetable) < 2:
+  result = ""
+else: result = vegetable[:2] + vegetable[-2:]
+
+print(result)
+
+# Task 2
+
+# Task 3
