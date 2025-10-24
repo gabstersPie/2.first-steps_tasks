8f318761f70cb3f7c4abe060a49af9f49a9e9a5e
gabstersPie <gabriella.iofe@protonmail.com>
2025-10-24 18:02:16 +0200

    created a program that checks id the input is valid, considering length and digits

--- booleans_loops/homewoek.py
+++ booleans_loops/homewoek.py
@@ -1,11 +1,18 @@
 # Task 1
-vegetable = input("enter your favorite vegetable here:")
-if len(vegetable) < 2:
-  result = ""
-else: result = vegetable[:2] + vegetable[-2:]
+# vegetable = input("enter your favorite vegetable here:")
+# if len(vegetable) < 2:
+#   result = ""
+# else: result = vegetable[:2] + vegetable[-2:]
 
-print(result)
+# print(result)
 
 # Task 2
+# phone_nr = input("on wich number can we call you? 10 digits: ")
+
+# if len(phone_nr) == 10 and phone_nr.isdigit():
+#   print("your nr is valid")
+# else:
+#   print("enter a valid phone number")
 
 # Task 3
+
