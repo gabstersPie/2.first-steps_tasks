11c87ed68cfe4cb7492f997c0b26b31747c71edd
gabstersPie <gabriella.iofe@protonmail.com>
2025-10-24 18:27:29 +0200

    no changes, just testing

--- booleans_loops/homewoek.py
+++ booleans_loops/homewoek.py
@@ -1,12 +1,13 @@
 # Task 1
-# vegetable = input("enter your favorite vegetable here:")
-# if len(vegetable) < 2:
-#   result = ""
-# else: result = vegetable[:2] + vegetable[-2:]
+vegetable = input("enter your favorite vegetable here: ")
 
-# print(result)
+if len(vegetable) < 2:
+  result = ""
+else: result = vegetable[:2] + vegetable[-2:]
 
-# Task 2
+print(result)
+
+# # Task 2
 # phone_nr = input("on wich number can we call you? 10 digits: ")
 
 # if len(phone_nr) == 10 and phone_nr.isdigit():
@@ -14,12 +15,12 @@
 # else:
 #   print("enter a valid phone number")
 
-# Task 3
-my_name = "gaby"
-user_name = input("whats your name? ").strip() #strip is to remove other unnecessary characters like space
+# # Task 3
+# my_name = "gaby"
+# user_name = input("whats your name? ").strip() #strip is to remove other unnecessary characters like space
 
-if user_name.lower() == my_name:
-  print(True)
-else:
-  print(False)
+# if user_name.lower() == my_name:
+#   print(True)
+# else:
+#   print(False)
 
